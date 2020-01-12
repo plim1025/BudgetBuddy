f622bfa8a9f569bd5aefc966b9155bfd2b99a1e9
plim1025 <plim1025@gmail.com>
2020-01-12 00:30:08 -0800

    Finished login functionality

--- asn1.cpp
+++ asn1.cpp
@@ -25,7 +25,7 @@ struct transaction {
 };
 
 void check_valid_input(int argc, char **argv, string *user_file_name, string *bud_file_name);
-string get_file_name(string file_name = "");
+string get_file_name(string file_type, string file_name = "");
 bool test_file(string file);
 
 user* create_users(int num_users);
@@ -40,18 +40,12 @@ user login(user *user_arr, int num_users);
 int check_login(user current_user, user *user_arr, int num_users);
 
 int main(int argc, char **argv) {
-    user current_user;
-    int id;
-
     int num_users;
     int num_buds;
-
     user *user_arr;
     budget *budget_arr;
-
     string user_file_name;
     string bud_file_name;
-    int username_line;
 
     // Error handling for command line arguments and file existence
     check_valid_input(argc, argv, &user_file_name, &bud_file_name);
@@ -67,84 +61,37 @@ int main(int argc, char **argv) {
     // Parse user/budget data and store in user/budget array
     get_user_data(user_arr, num_users, user_file);
     get_budget_data(budget_arr, num_buds, budget_file);
-    
-    login(user_arr, num_users);
+
+    user current_user = login(user_arr, num_users);
+    cout << current_user.id << " " << current_user.name << " " << current_user.password << endl;
 
     return 0;
-}
-
-user login(user *user_arr, int num_users) {
-    user current_user;
-    int login_attempts = 0;
-    int username_line;
-    
-    while(login_attempts < 3 && username_line != -1) {
-        cout << "Enter username: ";
-        cin >> current_user.name;
-        cout << "Enter password: ";
-        cin >> current_user.password;
-        username_line = check_login(current_user, user_arr, num_users);
-        login_attempts++;
-    }
-    // Exit program if login attempts exceeded 3
-    if(login_attempts > 3) {
-        cout << "Too many login attempts. Exiting program..." << endl;
-        exit(EXIT_FAILURE);
-    }
-    current_user.id = user_arr[username_line].id;
-    return current_user;
-}
-
-// Returns line number that password and username were found on if they match, otherwise returns -1
-int check_login(user current_user, user *user_arr, int num_users) {
-    for(int i = 0; i < num_users; i++) {
-        if(current_user.name == user_arr[i].name && current_user.password == user_arr[i].password)
-            return i + 1;
-    }
-    return -1;
-}
-
-void print_budget_data(budget* budget_arr, int num_buds) {
-    for(int i = 0; i < num_buds; i++) {
-        cout << budget_arr[i].id << " ";
-        cout << budget_arr[i].balance << " ";
-        cout << budget_arr[i].num_transactions << endl;
-        for(int i = 0; i < budget_arr[i].num_transactions; i++) {
-            cout << budget_arr[i].t->amount << " ";
-            cout << budget_arr[i].t->date << " ";
-            cout << budget_arr[i].t->category << " ";
-            cout << budget_arr[i].t->description << endl;
-        }
-    }
 }
 
 // Check for 3 command line arguments and for valid file names
 void check_valid_input(int argc, char **argv, string *user_file_name, string *bud_file_name) {
     if(argc != 3) {
-        cout << "Wrong number of command line arguments, please enter user file name: ";
-        *user_file_name = get_file_name();
-        cout << "Now enter transaction file name: ";
-        *bud_file_name = get_file_name();
+        cout << "Wrong number of command line arguments" << endl;
+        *user_file_name = get_file_name("user");
+        *bud_file_name = get_file_name("budget");
     } else {
-        *user_file_name = get_file_name(argv[1]);
-        *bud_file_name = get_file_name(argv[2]);
+        *user_file_name = get_file_name("user", argv[1]);
+        *bud_file_name = get_file_name("budget", argv[2]);
     }
 }
 
 // If given file name exists, return it, otherwise prompt user for new file name until enter one that exists
-string get_file_name(string file_name) {
+string get_file_name(string file_type, string file_name) {
     string new_file_name = "";
     // If given file name is valid, return it
     if(test_file(file_name))
         return file_name;
     // Else, prompt for file name until valid
-    while(true) {
-        cin >> new_file_name;
-        if(test_file(new_file_name))
-            break;
-        else
-            cout << "Bad file name, please input again: ";
-    }
+    do {
+        cout << "Bad " << file_type << " file name, please input again: ";
+        getline(cin, new_file_name);
+    } while(!test_file(new_file_name));
+
     return new_file_name;
 }
 
@@ -223,4 +170,39 @@ void get_transaction_data(transaction* transactions_arr, int num_trans, ifstream
         transactions_arr[i].description = description;
         transactions_arr[i].category = category;
     }
+}
+
+user login(user *user_arr, int num_users) {
+    user current_user;
+    int login_attempts = 0;
+    int username_line = -1;
+
+    while(login_attempts < 3) {
+        cout << "Enter username: ";
+        getline(cin, current_user.name);
+        cout << "Enter password: ";
+        getline(cin, current_user.password);
+        username_line = check_login(current_user, user_arr, num_users);
+        if(username_line != -1)
+            break;
+        else
+            cout << "Incorrect, login info, try again." << endl;
+        login_attempts++;
+    }
+    // Exit program if login attempts exceeded 1
+    if(login_attempts > 2) {
+        cout << "Too many login attempts. Exiting program..." << endl;
+        exit(EXIT_FAILURE);
+    }
+    current_user.id = user_arr[username_line].id;
+    return current_user;
+}
+
+// Returns line number that password and username were found on if they match, otherwise returns -1
+int check_login(user current_user, user *user_arr, int num_users) {
+    for(int i = 0; i < num_users; i++) {
+        if(current_user.name == user_arr[i].name && current_user.password == user_arr[i].password)
+            return i;
+    }
+    return -1;
 }
